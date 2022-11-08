712b1a00419e8da0a0bc7b99372228968252969b
Songju Kang <sonkang@c7r8s4.42seoul.kr>
2022-11-08 21:56:18 +0900

    ^^

--- srcs/requirements/mariadb/tools/mariadb_setting.sh
+++ srcs/requirements/mariadb/tools/mariadb_setting.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-service mysql start
+service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-# mysql $MYSQL_DATABASE -u root < ./wp_backup.sql
-mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
-# mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
-
-service mysql stop 
+mysql -e "CREATE DATABASE IF NOT EXISTS $MARIA_DATABASE";
+mysql -e "CREATE USER IF NOT EXISTS '$MARIA_USER'@'%' IDENTIFIED BY '$MARIA_PASSWORD'";
+mysql -e "GRANT ALL PRIVILEGES ON $MARIA_DATABASE.* TO '$MARIA_USER'@'%'";
+# mysql $MARIA_DATABASE -u root < ./wp_backup.sql
+mysql -e "ALTER USER '$MARIA_ROOT'@'localhost' IDENTIFIED BY '$MARIA_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+mysqladmin -u$MARIA_ROOT -p$MARIA_ROOT_PASSWORD shutdown
 
 exec mysqld
